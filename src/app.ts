0895b5065fda7c139910129718b7919d20ebba9c
henriqueyujiandrade <henrique_yujiandrade@hotmail.com>
2022-07-13 17:24:05 -0300

    feat: adicionado tratativa de erros

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import routes from './routers/routes';
+import "express-async-errors"
+import handleAppErrorMiddeware from './middlewares/handleAppError.middleware';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', routes);
+
+app.use(handleAppErrorMiddeware)
 
 export default app;
