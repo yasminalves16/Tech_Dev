2d1d5e40f121c0822af114bc67e4dfe002d9bb79
Lestar Angelo <lestarherminio@gmail.com>
2022-07-15 18:40:57 -0300

    heroku config 6

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from "./data-source"
 	await AppDataSource.initialize().catch(err => {
 		console.error("Error during Data Source initialization", err)
 	})
-
+	
+	app.listen(process.env.PORT || 3000, () => console.log("Running at http://localhost:3000"))
 	
 })()
