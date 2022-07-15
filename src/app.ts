ea346ef8eb53cd34f416b4357e85d3ecafe1aa3f
Lestar Angelo <lestarherminio@gmail.com>
2022-07-15 19:03:30 -0300

    heroku config 7

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { AppDataSource } from './data-source';
 import express from "express";
 import userRoutes from "./routers/users.routes";
 import loginRoute from "./routers/login.routes";
@@ -9,20 +10,25 @@ import commentRoutes from "./routers/comments.routes";
 
 const app = express();
 
-app.use(express.json());
+AppDataSource.initialize().then(async()=>{
+  
+    app.use(express.json());
 
-app.use("/users", userRoutes);
+    app.use("/users", userRoutes);
 
-app.use("/login", loginRoute);
+    app.use("/login", loginRoute);
 
-app.use("/media", imageRoute);
+    app.use("/media", imageRoute);
 
-app.use("/posts", postsRoutes);
+    app.use("/posts", postsRoutes);
 
-app.use("/comments", commentRoutes)
+    app.use("/comments", commentRoutes);
 
-app.use(handleAppErrorMiddeware);
+    app.use(handleAppErrorMiddeware);
 
+    app.listen(process.env.PORT || 3000, () => console.log("Running at http://localhost:3000"))
+
+}).catch((error)=>console.log(error))
 
 
 export default app;
