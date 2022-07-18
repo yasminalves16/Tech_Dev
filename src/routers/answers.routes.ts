110426ab2e315427cb129819dae4d1ad0576d6b2
Phtbl <pedrohtbl@gmail.com>
2022-07-18 14:15:48 -0300

    feat: list answer

--- src/routers/answers.routes.ts
+++ src/routers/answers.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import createAnswerController from "../controllers/answer/createAnswer.controller";
+import listAnswerController from "../controllers/answer/listAnswer.controller";
 import verifyToken from "../middlewares/verifyToken.middleware";
 
 const answerRoutes = Router();
 
 answerRoutes.post("/:commentId", verifyToken, createAnswerController )
-answerRoutes.get("/:commentId", verifyToken, )
-answerRoutes.delete("/:commentId", verifyToken,)
-answerRoutes.patch("/:commentId",  verifyToken,)
+answerRoutes.get("/:commentId", verifyToken, listAnswerController)
+answerRoutes.delete("/:answerId", verifyToken,)
+answerRoutes.patch("/:answerId",  verifyToken,)
 
 export default answerRoutes;
