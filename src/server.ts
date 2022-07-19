6f29fc4eaca9ae86c7450d55eebdb007f4361605
Lestar Angelo <lestarherminio@gmail.com>
2022-07-19 12:57:28 -0300

    merge Developer config app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import app from "./app"
-import { AppDataSource } from "./data-source"
+// import app from "./app"
+// import { AppDataSource } from "./data-source"
 
-(async () => {
-	await AppDataSource.initialize().catch(err => {
-		console.error("Error during Data Source initialization", err)
-	})
-
-	app.listen(process.env.PORT || 3000, () => console.log("Running at http://localhost:3000"))
-})()
+// (async () => {
+// 	await AppDataSource.initialize().catch(err => {
+// 		console.error("Error during Data Source initialization", err)
+// 	})
+	
+// })()
