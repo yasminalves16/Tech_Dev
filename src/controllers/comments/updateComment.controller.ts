5acb17495b1f01fcf8481e7540c72a63b8ddbb22
Yasmin <yasmin.alves16@outlook.com.br>
2022-07-20 15:03:07 -0300

    fix: update comments

--- src/controllers/comments/updateComment.controller.ts
+++ src/controllers/comments/updateComment.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import updateCommentService from "../../services/comments/updateComment.service";
+import { instanceToPlain } from "class-transformer"
 
 
 const updateCommentController = async (req: Request, res: Response, next: NextFunction ) =>{
@@ -11,7 +12,7 @@ const updateCommentController = async (req: Request, res: Response, next: NextFu
 
       const updateComment = await updateCommentService(id, description, userId)
   
-      return res.status(201).json(updateComment)
+      return res.status(201).json(instanceToPlain(updateComment))
   
     } catch (error) {
       next(error)
